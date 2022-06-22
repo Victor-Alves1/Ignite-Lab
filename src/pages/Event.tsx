12c20fca868d1d557ec400d463debdb393a9b9fd
Victor Alves da Costa <victoralves741@gmail.com>
2022-06-22 14:55:25 -0300

    aula 01

---
+++ src/pages/Event.tsx
@@ -0,0 +1,21 @@
+import { Header } from "../components/Header"
+import { Lesson } from "../components/Lesson"
+import { Sidebar } from "../components/Sidebar"
+import { Video } from "../components/Video"
+
+
+function Event() {
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+          <Video />
+          <Sidebar />
+      </main>
+    </div>
+   
+  )
+}
+
+export default Event
